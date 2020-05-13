b53be7b46e485bc7e1dffebcdbe38a0fc213c29f
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-13 03:38:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ function BoardMember(name, homeState, training) {
 BoardMember.prototype.veto = () => "No, I must disagree";
 BoardMember.prototype.approve = () => "You can do that!";
 BoardMember.prototype.doCharity = () => "I like to help people.";
-BoardMember.prototype.releasePressStatement = () => ""
+BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber.";
+BoardMember.prototype.sayHi = () => ""
